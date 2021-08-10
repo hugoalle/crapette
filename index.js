b06a0e5f64b760d075a6af1a0e6199d933bbe85a
= <hugoallemand41@hotmail.com>
2021-08-10 17:58:21 +0200

    connection and ejs

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const express = require('express');
+let ejs = require('ejs');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.set("view engine", 'ejs');
+
+let users = [];
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index', {users: users.map(u => u.name)});
   });
 
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
     console.log('a user connected with id : ' + socket.id);
+
+    socket.on("new-connection", (name) => {
+        handleNewConnection(socket, name);
+    })
 });
-  
+
+let handleNewConnection = function(socket, name) {
+    if (users.length >= 2) {
+        socket.emit('too-many-connected');
+    }
+    else {
+        users.push({socket: socket, name: name });
+        io.emit('display-connected-client', users.map(u => u.name));
+      
+        //send html to the client after countdown
+    }
+}  
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
--- public/script.js
+++ public/script.js
@@ -1,10 +1,29 @@
 
 let form = document.getElementById("form");
+let button = document.getElementById("button");
+let input = document.getElementById("input");
+
 form.addEventListener('submit', e => {
     e.preventDefault();
     handleConnection(e);
+    button.disabled = true;
 })        
 
 let handleConnection = function(e) {
     let socket = io();
+    socket.on("connect", () => {
+        socket.emit("new-connection", input.value);
+    })
+
+    socket.on('display-connected-client', arrayName => {
+        for (let i = 0; i<arrayName.length; i++) {
+            let user = i == 0 ? document.getElementById("user1") : document.getElementById("user2");
+            user.innerText = `${arrayName[i]} is connected`;
+        }   
+    });
+
+    socket.on('too-many-connected', () => {
+        window.alert("there is too many players for now, please try later when a slot is available");
+    });
+
 }
