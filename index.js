17ddbeaee49167f2380c3f119ef3534f8eb780e1
= <hugoallemand41@hotmail.com>
2021-08-11 16:20:53 +0200

    mise en place lancement du jeu, reste à gérer deconnexion pendant le chargement

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-let users = [];
+let players = [];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -16,25 +16,58 @@ app.use(express.static('public'))
 io.on('connection', (socket) => {
     console.log('a user connected with id : ' + socket.id);
 
-    io.to(socket.id).emit('display-connected-clients', users.map(u => u.name));
+    io.to(socket.id).emit('display-connected-clients', players.map(u => u.name));
 
     socket.on("playerSubmit", (name) => {
         handleNewPlayer(socket, name);
     });
 
+    socket.on('disconnect', function() {
+        console.log("socket with id " + socket.id + " disconnected");
+
+        //get the player that disconnected, with socket object
+        let n = null;
+        for (let i=0; i<players.length; i++) {
+            let player = players[i];
+            if (player.socket == socket) {
+                n = i;
+            }
+        }
+        //remove it from players array
+        players.splice(n, 1);
+        //redisplay connected clients
+        io.emit('display-connected-clients', players.map(u => u.name));
+    })
+
 })
 
 let handleNewPlayer = function(socket, name) {
 
-    if (users.length === 2) {
+    if (players.length === 2) {
         socket.emit('too-many-connected');
     }
     else {
-        users.push({socket: socket, name: name });
-        io.emit('display-connected-clients', users.map(u => u.name));
+        players.push({socket: socket, name: name });
+        io.emit('display-connected-clients', players.map(u => u.name));
+    }
+
+    if (players.length == 2) {
+
+        //we put the 2 players in the same room
+        players.forEach(v => {
+            v.socket.join("room1");
+        });
+        //we launch the countdown, on these client's page
+        io.to('room1').emit('decompte');
+
+        //in 5 sec, if the two players are still connected, we launch the game
+        setTimeout(function() {
+            if (players.length == 2) {
+                //launchGame(players[0], players[1]);
+            }
+        },3000)
     }
 }
-
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
--- public/script.js
+++ public/script.js
@@ -14,9 +14,16 @@ socket.on("connect", () => {
 });
 
 socket.on('display-connected-clients', arrayName => {
-    for (let i = 0; i<arrayName.length; i++) {
+    for (let i = 0; i<2; i++) {
+
         let user = i == 0 ? document.getElementById("user1") : document.getElementById("user2");
-        user.innerText = `${arrayName[i]} is connected`;
+
+        if (arrayName[i] !== undefined) {
+            user.innerText = `${arrayName[i]} is connected`;
+        }
+        else {
+            user.innerText = '';
+        }
     }
 });
 
@@ -25,3 +32,35 @@ socket.on('too-many-connected', () => {
     window.alert("there is too many players for now, please try later when a slot is available");
     button.disabled = false;
 });
+
+socket.on('decompte', () => {
+    let divDecompte = document.createElement("div");
+    divDecompte.style["border"] = "solid";
+    divDecompte.style["width"] = "50px";
+    divDecompte.style["height"] = "50px";
+    divDecompte.innerText = "3";
+    document.body.appendChild(divDecompte);
+    let timer = function(n) {
+        setTimeout( () => {
+            divDecompte.innerText = (n-1).toString();
+            n--;
+            if (n>0) {
+                timer(n);
+            }
+            else {
+                displayGame();
+            }
+        },1000
+        );
+    }
+    timer(3);
+});
+
+
+let displayGame = function() {
+    let boardgame = document.createElement("div");
+    boardgame.style["border"] = "solid";
+    boardgame.style["width"] = "1200px";
+    boardgame.style["height"] = "600px";
+    document.body.appendChild(boardgame);
+}
